9cf1e730182453cea643d9fa96cf52abce131628
iBlameMattheww <matthewobrien2023@gmail.com>
2026-09-05 18:14:57 -0400

    post op shift test

--- model/tests/test_wopr8_cpu.py
+++ model/tests/test_wopr8_cpu.py
@@ -57,3 +57,39 @@ def test_add_raises_carry():
     assert cpu.pc == 1
     assert cpu.get_flag(PSR_Z) == 0
     assert cpu.get_flag(PSR_C) == 1
+
+
+def test_add_post_shift():
+    cpu = WOPR_8()
+
+    cpu.regs[1] = 4
+    cpu.regs[2] = 5
+
+    cpu.rom[0] = encode_instruction_r(OP_ADD, rd = 1, rs = 2, shamt = 2, dir = 0)
+
+    cpu.step()
+
+    assert cpu.regs[1] == 36
+    assert cpu.pc == 1
+    assert cpu.get_flag(PSR_Z) == 0
+    assert cpu.get_flag(PSR_C) == 0
+    
+    cpu.regs[2] = 0 # poor man's shift immediate
+
+    cpu.rom[1] = encode_instruction_r(OP_ADD, rd = 1, rs = 2, shamt = 2, dir = 1)
+
+    cpu.step()
+
+    assert cpu.regs[1] == 9
+    assert cpu.pc == 2
+    assert cpu.get_flag(PSR_Z) == 0
+    assert cpu.get_flag(PSR_C) == 0
+
+    cpu.rom[2] = encode_instruction_r(OP_ADD, rd = 1, rs = 2, shamt = 5, dir  = 0)
+
+    cpu.step()
+
+    assert cpu.regs[1] == 32
+    assert cpu.pc == 3
+    assert cpu.get_flag(PSR_Z) == 0
+    assert cpu.get_flag(PSR_C) == 1
